7d671e84798c96fcdce2221440de636d7fd018a6
fmtabbara <fmtabbara@hotmail.co.uk>
2025-09-11 10:06:30 +0100

    Enhance pull request display by adding links to titles in closed and open pages

--- mergeboard/src/app/pulls/closed/page.tsx
+++ mergeboard/src/app/pulls/closed/page.tsx
@@ -11,8 +11,12 @@ export default async function GithubTest() {
     <div>
       <h1>My Github Repos</h1>
       <ul>
-        {pulls.data.map((pull) => (
-          <li key = {pull.id}>{pull.title}</li>
+          {pulls.data.map((pull) => (
+          <li key={pull.id}>
+            <a href={pull.html_url} target="_blank" rel="noopener noreferrer">
+              {pull.title}
+            </a>
+          </li>
         ))}
       </ul>
     </div>
--- mergeboard/src/app/pulls/open/page.tsx
+++ mergeboard/src/app/pulls/open/page.tsx
@@ -12,7 +12,11 @@ export default async function GithubTest() {
       <h1>My Github Repos</h1>
       <ul>
         {pulls.data.map((pull) => (
-          <li key = {pull.id}>{pull.title}</li>
+          <li key={pull.id}>
+            <a href={pull.html_url} target="_blank" rel="noopener noreferrer">
+              {pull.title}
+            </a>
+          </li>
         ))}
       </ul>
     </div>
