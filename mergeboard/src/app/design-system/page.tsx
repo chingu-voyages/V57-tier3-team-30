0016202a4c1ddfbe8289dd11d3652200dcdb3872
fmtabbara <fmtabbara@hotmail.co.uk>
2025-09-13 12:23:25 +0100

    add shadcn and start typography components

--- mergeboard/src/app/design-system/page.tsx
+++ mergeboard/src/app/design-system/page.tsx
@@ -1,15 +1,24 @@
-import { PageWrapper } from "../components/PageWrapper";
-import Swatch from "../components/Swatch";
+import { PageWrapper } from "@/app/components/layouts/PageWrapper";
+import Swatch from "@/app/components/Swatch";
+import {
+  Body1,
+  Body2,
+  Caption,
+  Headline,
+  HeadlineXL,
+  Label,
+  Subheading,
+} from "../components/typography";
 
 export default function Page() {
   return (
     <PageWrapper>
       <main className="ds-container">
-        <h1 className="text-4xl font-bold mb-5">Design system</h1>
+        <h1 className="text-4xl font-bold mb-8">Design system</h1>
         <section className="mb-8">
-          <h2 className="text-2xl font-semibold mb-3">Color palette</h2>
+          <h2 className="text-2xl font-semibold mb-3 ">Color palette</h2>
           <div className="max-w-3xl">
-            <div className="p-4">
+            <div className="mb-6">
               <h3 className="font-semibold mb-3 border-b-2">Primary</h3>
               <div className="flex gap-4 flex-wrap">
                 <Swatch color="bg-primary-green-100" value="#2dba4e" />
@@ -19,7 +28,7 @@ export default function Page() {
                 <Swatch color="bg-primary-white-100" value="#2dba4e" />
               </div>
             </div>
-            <div className="p-4">
+            <div className="mb-6">
               <h3 className="font-semibold mb-3 border-b-2">Secondary</h3>
               <div className="flex gap-4 flex-wrap">
                 <Swatch color="bg-secondary-green-300" value="#e6f4ea" />
@@ -27,6 +36,22 @@ export default function Page() {
                 <Swatch color="bg-secondary-green-900" value="#e6f4ea" />
               </div>
             </div>
+          </div>
+        </section>
+        <section>
+          <h2 className="text-2xl font-semibold mb-3">Typography</h2>
+          <div className="max-w-3xl flex flex-col gap-4">
+            <HeadlineXL>Headline XL</HeadlineXL>
+            <Headline>Headline</Headline>
+            <Subheading>Subheading</Subheading>
+            <Body1>
+              Body 1 - Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </Body1>
+            <Body2>
+              Body 2 - Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </Body2>
+            <Caption>Caption - Lorem ipsum dolor sit amet.</Caption>
+            <Label>Label - Lorem ipsum dolor sit amet.</Label>
           </div>
         </section>
       </main>
