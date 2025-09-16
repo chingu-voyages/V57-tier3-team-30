7ef92bb206c0d3b27de20d1b770f746d582a1da7
fmtabbara <fmtabbara@hotmail.co.uk>
2025-09-16 13:07:54 +0100

    [refactor] Update getPullRequests function to remove defaults and enhance PullRequest component layout

--- mergeboard/src/app/actions/getPullRequests.ts
+++ mergeboard/src/app/actions/getPullRequests.ts
@@ -5,26 +5,23 @@ import { components } from "@octokit/openapi-types";
 
 type Pull = components["schemas"]["pull-request"]
 
-const DEFAULTS = {
-  owner: "chingu-voyages",
-  repo: "V57-tier3-team-30",
-}
+
 
 function getPullRequests({
-  owner = DEFAULTS.owner,
-  repo = DEFAULTS.repo,
+  owner,
+  repo,
   sort,
   state,
   per_page = 10,
   page = 1,
 }: {
-  owner?: string
-  repo?: string
+  owner: string
+  repo: string
   sort?: "created" | "updated" | "popularity" | "long-running"
   state?: "open" | "closed" | "all"
   per_page?: number
   page?: number
-} = {}): Promise<{ data: Pull[] }> {
+}): Promise<{ data: Pull[] }> {
   const queryParams = `?sort=${sort}&state=${state}&per_page=${per_page}&page=${page}`
   const pullRequests = octokit.request(`GET /repos/${owner}/${repo}/pulls${queryParams}`)
   return pullRequests
--- mergeboard/src/app/components/pullRequest/index.tsx
+++ mergeboard/src/app/components/pullRequest/index.tsx
@@ -1,7 +1,6 @@
 import { Badge } from "@/components/ui/badge";
 import Card from "../card";
-import { PageWrapper } from "../layouts/PageWrapper";
-import { Body1, Body2, Subheading1 } from "../typography";
+import { Body2, Subheading1 } from "../typography";
 import { Button } from "@/components/ui/button";
 import { GitPullRequestIcon } from "lucide-react";
 
@@ -22,37 +21,35 @@ export const PullRequest = ({
   reviewers?: string[];
 }) => {
   return (
-    <PageWrapper>
-      <Card
-        className="w-md"
-        title={
-          <Subheading1>
-            #{PRNumber}: {title}
-          </Subheading1>
-        }
-        action={
-          <Badge className="bg-primary dark:bg-primary [&>svg]:size-5 rounded-3xl px-4 py-1">
-            <GitPullRequestIcon />
-            <Body2>Open</Body2>
-          </Badge>
-        }
-        content={
-          <div>
-            <Body2>Created by {CreatedBy}</Body2>
-            <Body2>Created at {new Date(CreatedAt).toLocaleDateString()}</Body2>
-            <Body2>Reviewers: {reviewers?.join(", ") || "None"}</Body2>
-          </div>
-        }
-        footer={
-          <div className="flex justify-end w-full">
-            <Button variant={"link"} asChild className="mt-2">
-              <a href={url} target="_blank" rel="noopener noreferrer">
-                View on GitHub
-              </a>
-            </Button>
-          </div>
-        }
-      />
-    </PageWrapper>
+    <Card
+      className="w-md"
+      title={
+        <Subheading1>
+          #{PRNumber}: {title}
+        </Subheading1>
+      }
+      action={
+        <Badge className="bg-primary dark:bg-primary [&>svg]:size-5 rounded-3xl px-4 py-1">
+          <GitPullRequestIcon />
+          <Body2>Open</Body2>
+        </Badge>
+      }
+      content={
+        <div>
+          <Body2>Created by {CreatedBy}</Body2>
+          <Body2>Created at {new Date(CreatedAt).toLocaleDateString()}</Body2>
+          <Body2>Reviewers: {reviewers?.join(", ") || "None"}</Body2>
+        </div>
+      }
+      footer={
+        <div className="flex justify-end w-full">
+          <Button variant={"link"} asChild className="mt-2">
+            <a href={url} target="_blank" rel="noopener noreferrer">
+              View on GitHub
+            </a>
+          </Button>
+        </div>
+      }
+    />
   );
 };
