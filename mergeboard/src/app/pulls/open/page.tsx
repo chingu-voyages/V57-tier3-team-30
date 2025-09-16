95c1c8dec29d213f173a19f8ca4effba95d7517d
fmtabbara <fmtabbara@hotmail.co.uk>
2025-09-16 12:04:10 +0100

    start open PRs page

--- mergeboard/src/app/pulls/open/page.tsx
+++ mergeboard/src/app/pulls/open/page.tsx
@@ -1,25 +1,31 @@
 import { getPullRequests } from "@/app/actions/getPullRequests";
-
-
+import { PullRequest } from "@/app/components/pullRequest";
 
 export default async function GithubTest() {
-
   const pulls = await getPullRequests({
-    state: "open"
+    state: "open",
   });
+  console.log(pulls);
+
   return (
     <div>
       <h1>My Github Repos</h1>
       <ul>
         {pulls.data.map((pull) => (
-          <li key={pull.id}>
-            <a href={pull.html_url} target="_blank" rel="noopener noreferrer">
-              {pull.title}
-            </a>
-          </li>
+          <PullRequest
+            key={pull.id}
+            PRNumber={pull.number}
+            title={pull.title}
+            CreatedBy={pull.user?.login || "Unknown"}
+            CreatedAt={pull.created_at}
+            status={pull.state as "open" | "closed"}
+            url={pull.html_url}
+            reviewers={pull.requested_reviewers?.map(
+              (reviewer) => reviewer.login
+            )}
+          />
         ))}
       </ul>
     </div>
   );
 }
-
