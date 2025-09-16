998adf1238762ac9199d0295b16e6b169610d285
fmtabbara <fmtabbara@hotmail.co.uk>
2025-09-16 13:07:54 +0100

    [refactor] Update getPullRequests function to remove defaults and enhance PullRequest component layout

--- mergeboard/src/app/pulls/open/page.tsx
+++ mergeboard/src/app/pulls/open/page.tsx
@@ -1,16 +1,32 @@
 import { getPullRequests } from "@/app/actions/getPullRequests";
+import { PageWrapper } from "@/app/components/layouts/PageWrapper";
 import { PullRequest } from "@/app/components/pullRequest";
+import { Headline, Subheading2 } from "@/app/components/typography";
+import { BookMarkedIcon, GitPullRequestArrow } from "lucide-react";
 
-export default async function GithubTest() {
+const DEFAULTS = {
+  owner: "chingu-voyages",
+  repo: "V57-tier3-team-30",
+};
+
+export default async function OpenPRsPage() {
   const pulls = await getPullRequests({
+    owner: DEFAULTS.owner,
+    repo: DEFAULTS.repo,
     state: "open",
   });
-  console.log(pulls);
 
   return (
-    <div>
-      <h1>My Github Repos</h1>
-      <ul>
+    <PageWrapper>
+      <div className="flex items-center gap-2 mb-8 justify-center">
+        <GitPullRequestArrow />
+        <Headline>Open Pull Requests</Headline>
+      </div>
+      <div className="flex items-center gap-1 mb-5">
+        <BookMarkedIcon className="inline size-6" />
+        <Subheading2 className="">{`${DEFAULTS.owner}/${DEFAULTS.repo}`}</Subheading2>
+      </div>
+      <ul className="flex flex-col gap-4">
         {pulls.data.map((pull) => (
           <PullRequest
             key={pull.id}
@@ -26,6 +42,6 @@ export default async function GithubTest() {
           />
         ))}
       </ul>
-    </div>
+    </PageWrapper>
   );
 }
