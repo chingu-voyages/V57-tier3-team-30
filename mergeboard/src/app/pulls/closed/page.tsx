8a0baf0cbb46839ba9a3f3eaa097e7a0867c01c8
fmtabbara <fmtabbara@hotmail.co.uk>
2025-09-19 00:09:31 +0100

    fix: Correctly map pulls data in GithubTest component

--- mergeboard/src/app/pulls/closed/page.tsx
+++ mergeboard/src/app/pulls/closed/page.tsx
@@ -11,7 +11,7 @@ export default async function GithubTest() {
     <div>
       <h1>My Github Repos</h1>
       <ul>
-        {pulls.data.map((pull) => (
+        {pulls.map((pull) => (
           <li key={pull.id}>
             <a href={pull.html_url} target="_blank" rel="noopener noreferrer">
               {pull.title}
