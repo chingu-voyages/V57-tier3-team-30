30d58a018df8f2449b59b6f65b0ad9113f91b57a
fmtabbara <fmtabbara@hotmail.co.uk>
2025-09-18 23:31:02 +0100

    get additional info for PRs and display

---
+++ mergeboard/src/app/actions/getPullRequestLastEvent.tsx
@@ -0,0 +1,34 @@
+import octokit from ".";
+
+export async function getLastPullRequestEvent({
+  owner,
+  repo,
+  pull_number,
+}: {
+  owner: string;
+  repo: string;
+  pull_number: number;
+}) {
+  // Get timeline events for the PR
+  const { data: timeline } = await octokit.request(
+    "GET /repos/{owner}/{repo}/issues/{issue_number}/timeline",
+    { owner, repo, issue_number: pull_number }
+  );
+
+  // Filter for relevant actions
+  const relevant = timeline.filter(({ event }) => {
+    if (!event) return false;
+    return [
+      "opened",
+      "commented",
+      "merged",
+      "review_requested",
+      "review_request_removed",
+      "reviewed",
+    ].includes(event);
+  });
+
+  const lastEvent = relevant[relevant.length - 1];
+
+  return lastEvent?.event?.replace(/_/g, " ");
+}
