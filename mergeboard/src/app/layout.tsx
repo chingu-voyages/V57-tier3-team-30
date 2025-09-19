5c1a4ad369fddb3c7e10cd402645b48ccec285dd
EslemOuederni <sousououederni@gmail.com>
2025-09-19 15:49:32 +0100

    add Footer component

--- mergeboard/src/app/layout.tsx
+++ mergeboard/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/app/providers/theme-provider";
 import Header from "@/components/layout/Header";
+import { Footer } from '@/components/layout/Footer';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <ThemeProvider>
           <Header />
           {children}
+          <Footer/>
         </ThemeProvider>
       </body>
     </html>
