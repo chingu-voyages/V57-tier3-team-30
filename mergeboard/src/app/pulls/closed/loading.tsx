8f3212ab51f35375c0ba21af81c4bbd970cc9128
skellynb <bisisina@gmail.com>
2025-09-22 15:05:06 +0100

    feat: update PR list to handle merged PRs

---
+++ mergeboard/src/app/pulls/closed/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <div className="flex flex-col items-center justify-center h-full">
+      <div className="loader mb-4"></div>
+      <p className="text-lg">Loading Closed Pull Requests...</p>
+    </div>
+  );
+}
