fcdccf3b92e714995e6a00a5fb14b85aa315b4e5
skellynb <bisisina@gmail.com>
2025-09-22 15:05:06 +0100

    feat: update PR list to handle merged PRs

--- mergeboard/src/app/pulls/closed/loading.tsx
+++ mergeboard/src/app/pulls/closed/loading.tsx
@@ -3,7 +3,7 @@ import { PullRequestSkeletons } from "@/app/components/pullRequest";
 export default function Loading() {
   return (
     <div className="flex flex-col gap-4 p-4">
-     <PullRequestSkeletons />
+      <PullRequestSkeletons />
     </div>
   );
 }
