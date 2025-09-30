7609406f7cca29ef8f50c3b2a706f799d9bfa924
EslemOuederni <sousououederni@gmail.com>
2025-09-30 19:08:16 +0100

    feat: add table and input components, integrate data-table for pr display

--- mergeboard/src/app/pulls/all/page.tsx
+++ mergeboard/src/app/pulls/all/page.tsx
@@ -1,28 +1,3 @@
-// import { PageWrapper } from "@/app/components/layouts/PageWrapper";
-// import { Headline, Subheading2 } from "@/app/components/typography";
-// import { DEFAULT_REPO } from "@/app/constants";
-// import { BookMarkedIcon, GitPullRequestArrow } from "lucide-react";
-// import PullRequests from "./components/PullRequests";
-// import { Suspense } from "react";
-// import { PullRequestSkeletons } from "@/app/components/pullRequest";
-
-// export default async function ClosedPRsPage() {
-//   return (
-//     <PageWrapper>
-//       <div className="flex items-center gap-2 mb-24">
-//         <GitPullRequestArrow />
-//         <Headline>Closed Pull Requests</Headline>
-//       </div>
-//       <div className="flex items-center gap-1 mb-5">
-//         <BookMarkedIcon className="inline size-6" />
-//         <Subheading2 className="">{`${DEFAULT_REPO.owner}/${DEFAULT_REPO.repo}`}</Subheading2>
-//       </div>
-//       <Suspense fallback={<PullRequestSkeletons />}>
-//         <PullRequests />
-//       </Suspense>
-//     </PageWrapper>
-//   );
-// }
 import { getPullRequests, mapPRs, Pull, PullsWithEvents} from "@/app/actions/getPullRequests"
 import { DEFAULT_REPO } from "@/app/constants"
 import { DataTable } from '../data-table'
@@ -34,7 +9,7 @@ export default async function PullRequestsPage() {
   const prs = await getPullRequests({
     owner: DEFAULT_REPO.owner,
     repo: DEFAULT_REPO.repo,
-    state: "closed",
+    state: "open",
   })
 
  await Promise.all(
@@ -58,4 +33,3 @@ export default async function PullRequestsPage() {
     </div>
   )
 }
-
