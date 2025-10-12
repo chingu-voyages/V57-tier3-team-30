8251e2bed1cbfcdc0421d5d92f7e91fdad8a3d74
fmtabbara <fmtabbara@hotmail.co.uk>
2025-10-12 18:16:12 +0100

    general fixes

--- mergeboard/src/app/page.tsx
+++ mergeboard/src/app/page.tsx
@@ -41,9 +41,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <EmailReminderPrompt />
-      <ReminderScheduler />
-
       <h2 className="text-center text-4xl pb-20 pt-9 font-semibold">
         MergeBoard allows you to
       </h2>
